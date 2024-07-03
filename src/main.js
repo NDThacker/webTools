999a88fdc1cc8edefdcb6449b836d638b46255ab
Naman <namanthacker1965@gmail.com>
2024-07-03 23:53:53 +0530

    Added simple routing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import CalcComp from './components/Calc.vue';
+import CurrencyComp from './components/CurrencyConverter.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: '/calc', component: CalcComp },
+		{ path: '/currency', component: CurrencyComp }
+	]
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
